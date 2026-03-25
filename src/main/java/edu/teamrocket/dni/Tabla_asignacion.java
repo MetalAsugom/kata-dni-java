36ea2d9acf4a8331aae6d77c41a259fb4169f8fa
MetalAsugom <xianmorales01@gmail.com>
2026-03-25 10:51:32 +0100

    feat(Tabla_asignacion):
    Add class Tabla_asignacion

---
+++ src/main/java/edu/teamrocket/dni/Tabla_asignacion.java
@@ -0,0 +1,40 @@
+package edu.teamrocket.dni;
+
+public class Tabla_asignacion {
+
+    private final char[] tabla = {'T', 'R', 'W', 'A', 'G', 'M',
+            'Y', 'F', 'P', 'D', 'X', 'B',
+            'N', 'J', 'Z', 'S', 'Q', 'V',
+            'H', 'L', 'C', 'K', 'E'};
+
+    Tabla_asignacion() {};
+
+    private int get_modulo() {
+        return this.tabla.length;
+    }
+
+    char calcular_letra(String DNI) {
+        int dni = Integer.parseInt(DNI);
+        int posicion = dni % get_modulo();
+        return get_letra(posicion);
+    }
+
+    char get_letra(int posicion) throws ArrayIndexOutOfBoundsException {
+
+        try {
+            return this.tabla[posicion];
+        }
+        catch (ArrayIndexOutOfBoundsException exception) {
+            throw new ArrayIndexOutOfBoundsException(
+                    "Posición fuera de los límites de la tabla de asignación"
+            );
+        }
+
+    }
+
+    boolean is_letra_permitida (char letra) {
+        String tabla_string = String.valueOf(tabla);
+        return tabla_string.contains(Character.toString(letra));
+    }
+
+}
